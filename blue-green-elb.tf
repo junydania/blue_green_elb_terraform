d32195935e780506546eeb2f19a6a4250dc18f3b
Tim Rourke <tim@timrourke.com>
2018-10-04 21:09:35 -0500

    Add ELB and target groups with listeners and attachments

--- blue-green-elb.tf
+++ blue-green-elb.tf
@@ -20,16 +20,11 @@ resource "aws_instance" "green_ec2" {
   }
 }
 
-resource "aws_elb" "blue_green_elb" {
+resource "aws_lb" "blue_green_elb" {
   name                = "blue-green-elb"
+  internal            = false
+  load_balancer_type  = "application"
   availability_zones  = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
-  "listener" {
-    instance_port     = 8000
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
 
   instances                   = ["${aws_instance.green_ec2.id}"]
   cross_zone_load_balancing   = true
@@ -39,5 +34,64 @@ resource "aws_elb" "blue_green_elb" {
 
   tags {
     Name = "blue-green-elb"
+  }
+}
+
+resource "aws_vpc" "blue_green_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags {
+    Name = "blue-green-vpc"
+  }
+}
+
+resource "aws_lb_target_group" "blue_elb_target_group" {
+  name        = "blue_ec2_elb_target_group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = "${aws_vpc.blue_green_vpc.id}"
+
+  tags {
+    Name = "blue-elb-target-group"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "blue_elb_target_group_attachment" {
+  target_group_arn = "${aws_lb_target_group.blue_elb_target_group.arn}"
+  target_id        = "${aws_instance.blue_ec2.id}"
+  port             = 80
+}
+
+resource "aws_lb_target_group" "green_elb_target_group" {
+  name        = "green_ec2_elb_target_group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = "${aws_vpc.blue_green_vpc.id}"
+
+  tags {
+    Name = "green-elb-target-group"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "green_elb_target_group_attachment" {
+  target_group_arn = "${aws_lb_target_group.green_elb_target_group.arn}"
+  target_id        = "${aws_instance.green_ec2.id}"
+  port             = 80
+}
+
+resource "aws_lb_listener" "blue_green_elb_listener" {
+  "default_action" {
+    type = "forward"
+    target_group_arn = "${aws_lb_target_group.green_elb_target_group.arn}"
+  }
+
+  load_balancer_arn = "${aws_lb.blue_green_elb.arn}"
+  port              = 80
+  protocol          = "HTTP"
+
+  tags = {
+    Name = "blue_green_elb_listener"
   }
 }
